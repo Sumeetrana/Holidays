500d845aadecf04d2671501f45556a33ced67030
Sumeetsinh Ketankumar Rana <sumeetsinh28@gmail.com>
2024-04-25 13:21:41 +0400

    feat: mongodb connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+
 import "dotenv/config"
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
 const app = express();
 app.use(express.json())
