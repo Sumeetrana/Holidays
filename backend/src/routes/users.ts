2a9c7ead309bf244c99ba432aed1fa9336e48048
Sumeetsinh Ketankumar Rana <sumeetsinh28@gmail.com>
2024-04-25 19:22:56 +0400

    feat: signup api validation added using express-validator

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,11 +1,25 @@
-import express from "express";
-import jwt from "jsonwebtoken"
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { check, validationResult } from 'express-validator';
 
 import User from "../models/user";
 
 const router = express.Router()
 
-router.post("/register", async (req, res) => {
+router.post("/register", [
+    check("firstName", "First name is required").isString(),
+    check("lastName", "Last name is required").isString(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password is required").isLength({
+        min: 6
+    }),
+], async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({message: errors.array()})
+    }
+
     const { email } = req.body;
     try {
         let user = await User.findOne({
