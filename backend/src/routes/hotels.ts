7bfb85e108c4c6af3d6e9c506b2d6aaaae45b4c1
Sumeetsinh Ketankumar Rana <sumeetsinh28@gmail.com>
2024-04-28 20:03:36 +0400

    feat(BE): get hotels route

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -57,4 +57,14 @@ router.post("/", verifyToken, [
      
 })
 
+router.get("/", verifyToken, async (req: Request, res: Response) => {
+    try {
+        const hotels = await Hotel.find({userId: req.userId});
+        res.json(hotels)
+    } catch(error) {
+        res.status(500).json({message: "Error fetching users!"})
+    }
+    
+})
+
 export default router;
