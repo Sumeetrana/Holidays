4371c594827f85ed662fe5a05edccc975fd88403
Sumeetsinh Ketankumar Rana <sumeetsinh28@gmail.com>
2024-04-29 00:05:30 +0400

    feat(BE): added update hotel by id api

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -7,7 +7,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 export const register = async (formData: RegisterFormData) => {
     const response = await fetch(`${API_BASE_URL}/api/users/register`, {
         method: 'POST',
-        // To enable http-cookies
+        // To enable http-cookies and send them to backend while calling APIs
         credentials: "include",
         headers: {
             "Content-Type": "application/json"
